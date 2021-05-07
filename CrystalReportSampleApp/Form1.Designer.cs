c788aa2fe42a9f7f82e276ff37e9b35b547bc1d3
suhaybnasir <suhaybabdinaasir@gmail.com>
2021-05-08 00:14:37 +0300

    Added one more DataSet and GridView

--- CrystalReportSampleApp/Form1.Designer.cs
+++ CrystalReportSampleApp/Form1.Designer.cs
@@ -31,9 +31,12 @@ namespace CrystalReportSampleApp
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.printButton = new System.Windows.Forms.Button();
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.CrystalReport11 = new CrystalReportSampleApp.CrystalReport1();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport12 = new CrystalReportSampleApp.CrystalReport1();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -56,15 +59,25 @@ namespace CrystalReportSampleApp
             this.printButton.UseVisualStyleBackColor = true;
             this.printButton.Click += new System.EventHandler(this.printButton_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(607, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(548, 444);
+            this.dataGridView2.TabIndex = 0;
+            // 
             // crystalReportViewer1
             // 
             this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(579, 12);
+            this.crystalReportViewer1.Location = new System.Drawing.Point(1176, 12);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.CrystalReport11;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(680, 497);
+            this.crystalReportViewer1.ReportSource = this.CrystalReport12;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(724, 444);
             this.crystalReportViewer1.TabIndex = 2;
             this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
@@ -72,13 +85,15 @@ namespace CrystalReportSampleApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1271, 538);
+            this.ClientSize = new System.Drawing.Size(1924, 538);
             this.Controls.Add(this.crystalReportViewer1);
             this.Controls.Add(this.printButton);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,8 +102,10 @@ namespace CrystalReportSampleApp
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button printButton;
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
         private CrystalReport1 CrystalReport11;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalReport1 CrystalReport12;
     }
 }
 
--- CrystalReportSampleApp/Form1.cs
+++ CrystalReportSampleApp/Form1.cs
@@ -11,23 +11,34 @@ namespace CrystalReportSampleApp
         {
             InitializeComponent();
             BindData();
+            BindData2();
         }
 
         private void BindData()
         {
             DataTable dt = new DataTable();
-            dt.Columns.Add("ID", typeof(Int16));
+            dt.Columns.Add("ID", typeof(string));
             dt.Columns.Add("Name", typeof(string));
             dt.Columns.Add("Result", typeof(string));
 
             dataGridView1.DataSource = dt;
         }
 
+        private void BindData2()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Lambar", typeof(string));
+            dt.Columns.Add("Magac", typeof(string));
+            dt.Columns.Add("Natiijo", typeof(string));
+
+            dataGridView2.DataSource = dt;
+        }
+
         private void printButton_Click(object sender, EventArgs e)
         {
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
-            dt.Columns.Add("ID", typeof(Int16));
+            dt.Columns.Add("ID", typeof(string));
             dt.Columns.Add("Name", typeof(string));
             dt.Columns.Add("Result", typeof(string));
             foreach (DataGridViewRow dgr in dataGridView1.Rows)
@@ -36,10 +47,28 @@ namespace CrystalReportSampleApp
             }
             ds.Tables.Add(dt);
             ds.WriteXmlSchema("Sample.xml");
+
+
+            DataSet ds1 = new DataSet();
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("Lambar", typeof(string));
+            dt1.Columns.Add("Magac", typeof(string));
+            dt1.Columns.Add("Natiijo", typeof(string));
+            foreach (DataGridViewRow dgr1 in dataGridView2.Rows)
+            {
+                dt.Rows.Add(dgr1.Cells[0].Value, dgr1.Cells[1].Value, dgr1.Cells[2].Value);
+            }
+            ds1.Tables.Add(dt1);
+            ds1.WriteXmlSchema("Sample1.xml");
+
+
             CrystalReport1 cr = new CrystalReport1();
-            cr.SetDataSource(ds);
+            cr.SetDataSource(ds1);
+
             crystalReportViewer1.ReportSource = cr;
             crystalReportViewer1.Refresh();
+
+
         }
     }
 }
